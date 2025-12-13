2fd940c3bc0e243c69bde53ac7e11161ca3ba754
nestor2312 <nestorcanal01@gmail.com>
2025-12-12 22:38:06 -0500

    rename model subcategory

--- app/Models/Subcategory.php
+++ app/Models/Subcategory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class SubCategory extends Model
+
 {
     use HasFactory;
     //  public function grupos()
