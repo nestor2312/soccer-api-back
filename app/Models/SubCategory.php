91ffabae4fe991ba0b9d65a9c05c2dcd46985e55
nestor2312 <nestorcanal01@gmail.com>
2025-12-13 13:45:44 -0500

    rename model subcategory

--- app/Models/SubCategory.php
+++ app/Models/SubCategory.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SubCategory extends Model
+class Subcategory extends Model
 {
     use HasFactory;
 
-      public function equipos()
+          public function equipos()
     {
         return $this->hasManyThrough(Equipo::class, Grupos::class, 'subcategoria_id', 'grupo_id');
     }
