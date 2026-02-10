00dcd376dcedb2691a95b1bd2ec80467cbdede82
nestor2312 <nestorcanal01@gmail.com>
2026-02-10 14:56:01 -0500

    lin cors

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://liga-dorada-panama-fubol.vercel.app', ],  // La dirección de tu aplicación React
+    'allowed_origins' => ['http://localhost:5173', 'link', ],  // La dirección de tu aplicación React
 
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
