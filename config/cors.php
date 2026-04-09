b48b9d9034d8d9808448fb66828694fc67ddca0d
nestor2312 <nestorcanal01@gmail.com>
2026-04-09 14:01:52 -0500

    vp

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
     'allowed_origins' => ['http://localhost:5173', 'https://campeonatos.fubolzona.com'],  // La dirección de tu aplicación React
-
+// ss
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
