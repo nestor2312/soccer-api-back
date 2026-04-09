7d533406d3d79100a9fb2e59fbd27d57cf67fd7a
nestor2312 <nestorcanal01@gmail.com>
2026-04-09 14:21:00 -0500

    vp 1

--- config/cors.php
+++ config/cors.php
@@ -4,7 +4,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173', 'https://campeonatos.fubolzona.com'],  // La dirección de tu aplicación React
+    'allowed_origins' => ['http://localhost:5173', 'https://mundialdeclubescanare.fubolzona.com/'],  // La dirección de tu aplicación React
 // ss
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
